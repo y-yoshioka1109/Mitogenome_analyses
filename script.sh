68f61be10ddbf9e8651caeb2f16ac54264ea14d4
Yuki Yoshioka <69947704+y-yoshioka1109@users.noreply.github.com>
2025-09-02 16:08:11 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,3 +6,7 @@ for i in `ls|grep .gff | awk -F"\.gff" '{print$1}'`;do perl gff2gbk.pl $i.gff > 
 for i in `ls|grep .gff | awk -F"\.gff" '{print$1}'`;do cat $i.fa | seqkit replace -s -p '(\w{10})' -r '$1 ' -w 66   \
   | perl -ne 'if (/^>/) {print; $n=1} else {s/ \r?\n$/\n/; printf "%9d %s", $n, $_; $n+=60;}' | grep -v ">" >> $i.gbk ;done
 
+#Clinkerの実行
+clinker Coelastrea_aspera.gbk Astrea_annuligera.gbk Paramontastraea_salebrosa.gbk \
+Blastomussa_merleti.gbk Blastomussa_wellsi.gbk Physogyra_lichtensteini.gbk Plerogyra_sinuosa.gbk \
+ -p plot.html --use_file_order -i 0.3 --jobs ${SLURM_CPUS_PER_TASK}
