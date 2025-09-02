aebc7d521b44c80ef82684c31b0a3b8548b164c0
Yuki Yoshioka <69947704+y-yoshioka1109@users.noreply.github.com>
2025-09-02 13:51:08 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #Clinkerを使ったシンテニー解析
 
 #genbankファイルの作成
-#for i in `ls|grep .gff | awk -F"\.gff" '{print$1}'`;do perl gff2gbk.pl $i.gff > $i.gbk ;done
-#for i in `ls|grep .gff | awk -F"\.gff" '{print$1}'`;do cat $i.fa | seqkit replace -s -p '(\w{10})' -r '$1 ' -w 66   \
+for i in `ls|grep .gff | awk -F"\.gff" '{print$1}'`;do perl gff2gbk.pl $i.gff > $i.gbk ;done
+for i in `ls|grep .gff | awk -F"\.gff" '{print$1}'`;do cat $i.fa | seqkit replace -s -p '(\w{10})' -r '$1 ' -w 66   \
   | perl -ne 'if (/^>/) {print; $n=1} else {s/ \r?\n$/\n/; printf "%9d %s", $n, $_; $n+=60;}' | grep -v ">" >> $i.gbk ;done
 
